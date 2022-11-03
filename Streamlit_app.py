e441fd787e891076d61a4c1680a06b27bddacea5
jigar2208 <117335326+jigar2208@users.noreply.github.com>
2022-11-03 11:16:10 +0530

    Update Streamlit_app.py
    Updated line with the help of code provided into the tutorials

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner') 
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & blueberry oatemeal')
+streamlit.text('Kale, spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled free-Range egg')
 
